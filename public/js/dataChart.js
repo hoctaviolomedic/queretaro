c44f91210f92e88aab9d9ba532e3f2a69f0de549
Octavio Carrillo <hector.carrillo@lomedic.com>
2017-09-04 13:45:03 -0500

    Cambios Smart, Botstraperizado

--- public/js/dataChart.js
+++ public/js/dataChart.js
@@ -1,39 +1,159 @@
-$(document).ready(function(){
-	var ctx = document.getElementById("myChart");
-	var myChart = new Chart(ctx, {
-	    type: 'bar', //tipo de chart
-	    data: {
-	        labels: ["IPEJAL", "SEGA", "Farmacia Rev", "GENE", "LoMedic", "TWITCH"],
-	        datasets: [{
-	            label: '$(k)',
-	            data: [12, 19, 3, 5, 2, 3],
-	            backgroundColor: [
-	                'rgba(255, 99, 132, 0.2)',
-	                'rgba(54, 162, 235, 0.2)',
-	                'rgba(255, 206, 86, 0.2)',
-	                'rgba(75, 192, 192, 0.2)',
-	                'rgba(153, 102, 255, 0.2)',
-	                'rgba(255, 159, 64, 0.2)'
-	            ],
-	            borderColor: [
-	                'rgba(255,99,132,1)',
-	                'rgba(54, 162, 235, 1)',
-	                'rgba(255, 206, 86, 1)',
-	                'rgba(75, 192, 192, 1)',
-	                'rgba(153, 102, 255, 1)',
-	                'rgba(255, 159, 64, 1)'
-	            ],
-	            borderWidth: 1
-	        }]
-	    },
-	    options: {
-	        scales: {
-	            yAxes: [{
-	                ticks: {
-	                    beginAtZero:true
-	                }
-	            }]
-	        }
-	    }
-	});
+var chart = AmCharts.makeChart( "pie", {
+"type": "pie",
+"theme": "light",
+"dataProvider": [ {
+  "country": "Lithuania",
+  "litres": 501.9
+}, {
+  "country": "Czech Republic",
+  "litres": 301.9
+}, {
+  "country": "Ireland",
+  "litres": 201.1
+}, {
+  "country": "UK",
+  "litres": 99
+}, {
+  "country": "Belgium",
+  "litres": 60
+}, {
+  "country": "The Netherlands",
+  "litres": 50
+} ],
+"valueField": "litres",
+"titleField": "country",
+ "balloon":{
+ "fixedPosition":true
+},
+"export": {
+  "enabled": true
+}
 });
+var chart2 = AmCharts.makeChart( "pie2", {
+"type": "pie",
+"theme": "light",
+"dataProvider": [ {
+  "country": "Lithuania",
+  "litres": 501.9
+}, {
+  "country": "Czech Republic",
+  "litres": 301.9
+}, {
+  "country": "The Netherlands",
+  "litres": 50
+} ],
+"valueField": "litres",
+"titleField": "country",
+ "balloon":{
+ "fixedPosition":true
+},
+"export": {
+  "enabled": true
+}
+});
+var chart3 = AmCharts.makeChart( "pie3", {
+"type": "pie",
+"theme": "light",
+"dataProvider": [ {
+  "country": "Lithuania",
+  "litres": 501.9
+}, {
+  "country": "Czech Republic",
+  "litres": 301.9
+}, {
+  "country": "The Netherlands",
+  "litres": 50
+} ],
+"valueField": "litres",
+"titleField": "country",
+ "balloon":{
+ "fixedPosition":true
+},
+"export": {
+  "enabled": true
+}
+});
+
+//Line Chart
+var chartData = generateChartData();
+var chart4 = AmCharts.makeChart("line", {
+    "type": "serial",
+    "theme": "light",
+    "marginRight": 80,
+    "autoMarginOffset": 20,
+    "marginTop": 7,
+    "dataProvider": chartData,
+    "valueAxes": [{
+        "axisAlpha": 0.2,
+        "dashLength": 1,
+        "position": "left"
+    }],
+    "mouseWheelZoomEnabled": true,
+    "graphs": [{
+        "id": "g1",
+        "balloonText": "[[value]]",
+        "bullet": "round",
+        "bulletBorderAlpha": 1,
+        "bulletColor": "#FFFFFF",
+        "hideBulletsCount": 50,
+        "title": "red line",
+        "valueField": "visits",
+        "useLineColorForBulletBorder": true,
+        "balloon":{
+            "drop":true
+        }
+    }],
+    "chartScrollbar": {
+        "autoGridCount": true,
+        "graph": "g1",
+        "scrollbarHeight": 40
+    },
+    "chartCursor": {
+       "limitToGraph":"g1"
+    },
+    "categoryField": "date",
+    "categoryAxis": {
+        "parseDates": true,
+        "axisColor": "#DADADA",
+        "dashLength": 1,
+        "minorGridEnabled": true
+    },
+    "export": {
+        "enabled": true
+    }
+});
+
+chart.addListener("rendered", zoomChart);
+zoomChart();
+
+// this method is called when chart is first inited as we listen for "rendered" event
+function zoomChart() {
+    // different zoom methods can be used - zoomToIndexes, zoomToDates, zoomToCategoryValues
+    chart.zoomToIndexes(chartData.length - 40, chartData.length - 1);
+}
+
+
+// generate some random data, quite different range
+
+// generate some random data, quite different range
+function generateChartData() {
+    var chartData = [];
+    var firstDate = new Date();
+    firstDate.setDate(firstDate.getDate() - 5);
+    var visits = 1200;
+    for (var i = 0; i < 1000; i++) {
+        // we create date objects here. In your data, you can have date strings
+        // and then set format of your dates using chart.dataDateFormat property,
+        // however when possible, use date objects, as this will speed up chart rendering.
+        var newDate = new Date(firstDate);
+        newDate.setDate(newDate.getDate() + i);
+        
+        visits += Math.round((Math.random()<0.5?1:-1)*Math.random()*10);
+
+        chartData.push({
+            date: newDate,
+            visits: visits
+        });
+    }
+    return chartData;
+}
--- public/js/empleados.js
+++
@@ -1,8 +0,0 @@
-function activar_infonavit() {
-
-    var on = $('#infonavit_activo:checked').val();
-    if ($('#infonavit_activo:checked').val() == 'on')
-    {$('#numero_infonavit').prop('disabled',false);}
-    else
-    {$('#numero_infonavit').prop('disabled',true);}
-}
