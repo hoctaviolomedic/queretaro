3aeb344d64c1ba1450ca09c188918511dadc982e
Octavio Carrillo <hector.carrillo@lomedic.com>
2017-09-04 18:08:05 -0500

    Smart Layouts Bootstraperizados

--- resources/views/layouts/smart/create.blade.php
+++ resources/views/layouts/smart/create.blade.php
@@ -3,35 +3,36 @@
 @section('title', currentEntityBaseName() . '@Agregar')
 
 @section('form-header')
-	{!! Form::open(['url' => companyRoute('index'), 'id' => 'form-model', 'class' => 'col s12 m12']) !!}
+	{!! Form::open(['url' => companyRoute('index'), 'id' => 'form-model', 'class' => 'col-sm-12']) !!}
 @endsection
 
 @section('form-actions')
-	<div class="row">
-		<div class="right">
-			{{ Form::button('Guardar', ['type' =>'submit', 'class'=>'waves-effect waves-light btn orange']) }}
-			{{ link_to(companyRoute('index'), 'Cerrar', ['class'=>'waves-effect waves-teal btn-flat teal-text']) }}
-		</div>
-	</div>
+<div class="text-right">
+	{{ Form::button('<span class="glyphicon glyphicon-floppy-disk" aria-hidden="true"></span> Guardar', ['type' =>'submit', 'class'=>'btn btn-danger']) }}
+	{{ link_to(companyRoute('index'), 'Cerrar', ['class'=>'btn btn-default']) }}
+</div>
 @endsection
 
-@section('form-title')
-	{{ HTML::tag('h4','Agregar '. str_singular(currentEntityBaseName())) }}
-@endsection
+@section('form-title', 'Agregar '. str_singular(currentEntityBaseName()))
 
 @section('content')
-<div class="row">
-	<div class="col @yield('content-width')">
-		@yield('form-header')
-    		<div class="row">
-    			@yield('form-title')
-    			@yield('form-actions')
-    			<fieldset @yield('fieldset')>
-    				@yield('form-content')
-    			</fieldset>
-    		</div>
-		{!! Form::close() !!}
-		@yield('form-utils')
+<br>
+<div class="panel shadow-3 panel-danger">
+	<div class="panel-heading">
+		<h3 class="panel-title text-center">@yield('form-title')</h3>
+	</div>
+	<div class="panel-body">
+		<div class="row">
+			<div class="@yield('content-width')">
+				@yield('form-header')
+				<fieldset @yield('fieldset')>
+					@yield('form-content')
+				</fieldset>
+				@yield('form-actions')
+				{!! Form::close() !!}
+				@yield('form-utils')
+			</div>
+		</div>
 	</div>
 </div>
 @endsection
--- resources/views/layouts/smart/edit.blade.php
+++ resources/views/layouts/smart/edit.blade.php
@@ -3,18 +3,14 @@
 @section('title', currentEntityBaseName() . '@Editar')
 
 @section('form-header')
-    {!! Form::open(['method'=>'put', 'url' => companyRoute('update'), 'id' => 'form-model', 'class' => 'col s12 m12']) !!}
+    {!! Form::open(['method'=>'put', 'url' => companyRoute('update'), 'id' => 'form-model', 'class' => 'col-sm-12']) !!}
 @endsection
 
 @section('form-actions')
-    <div class="row">
-        <div class="right">
-            {{ Form::button('Guardar', ['type' =>'submit', 'class'=>'waves-effect waves-light btn orange']) }}
-            {{ link_to(companyRoute('index'), 'Cerrar', ['class'=>'waves-effect waves-teal btn-flat teal-text']) }}
-        </div>
-    </div>
+<div class="text-right">
+    {{ Form::button('<span class="glyphicon glyphicon-floppy-disk" aria-hidden="true"></span> Guardar', ['type' =>'submit', 'class'=>'btn btn-danger']) }}
+    {{ link_to(companyRoute('index'), 'Cerrar', ['class'=>'btn btn-default']) }}
+</div>
 @endsection
 
-@section('form-title')
-    {{ HTML::tag('h4','Editar '. str_singular(currentEntityBaseName())) }}
-@endsection
+@section('form-title', 'Editar '. str_singular(currentEntityBaseName()))
