be80e4959cc761bfcc9c5783a855598dd264154d
Israel del Toro <israel.hernandez@lomedic.com>
2017-09-17 18:09:16 -0500

    Actualizacion requisiciones

--- public/js/requisicioneshospitalarias.js
+++ public/js/requisicioneshospitalarias.js
@@ -121,7 +121,9 @@ $('select[name="id_localidad"]').on('change', function() {
                 });
             }
         });
-
+        $('#id_area option[value="0"]');
+        $('#producto option[value="0"]');
+        $('#cantidad option[value="0"]');
     }else{
         $('select[name="id_area"]').empty();
         $('select[name="id_usuario_surtido"]').empty();
