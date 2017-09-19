dbb317f00fbc5cba6e1acb519e3b87cba79e2177
Héctor González <hectorgonzalezlomedic@abisalud.com>
2017-09-19 11:06:16 -0500

    Modificaciones ocasionadas por "Nandousuario" v5

--- resources/views/captura/recetas/surtir.blade.php
+++ resources/views/captura/recetas/surtir.blade.php
@@ -102,6 +102,7 @@
                                         @if(isset($detalle->fecha_surtido))
                                             @if($detalle->recurrente > 0 && DB::select("select extract(days from (timestamp 'now()' - timestamp '".$detalle->fecha_surtido."')) as diferencia")[0]->diferencia <= $detalle->recurrente && $detalle->veces_surtidas > 0 && ($detalle->cantidad_pedida*$detalle->veces_surtidas)/$detalle->cantidad_surtida)
                                                 <label>Producto surtido. Recuerda que la siguiente fecha es <b>{{DB::select("select date '" . $detalle->fecha_surtido . "' + integer '" . $detalle->recurrente . "' as diferencia")[0]->diferencia}}</b></label>
+                                                <input type="hidden" id="cantidadsurtir{{$detalle->id_receta_detalle}}" name="detalle[{{$detalle->id_receta_detalle}}][cantidadsurtir]" value="0">
                                             @elseif($detalle->veces_surtir > $detalle->veces_surtidas)
                                                     <input type="number" class="form-control number-only" min="0" placeholder="Ej: 6" id="cantidadsurtir{{$detalle->id_receta_detalle}}" name="detalle[{{$detalle->id_receta_detalle}}][cantidadsurtir]">
                                             @else
