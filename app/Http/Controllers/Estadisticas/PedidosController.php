890f3de153495edf560205f4ed41c1f45a0f0ec8
juanfrancolomedic <juan.franco@lomedic.com>
2017-09-19 12:14:16 -0500

    Modulo Estadisticas

---
+++ app/Http/Controllers/Estadisticas/PedidosController.php
@@ -0,0 +1,135 @@
+<?php
+namespace App\Http\Controllers\Estadisticas;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\ControllerBase;
+use App\Http\Models\Captura\Localidades;
+use App\Http\Models\Captura\Jurisdicciones;
+use DB;
+
+class PedidosController extends ControllerBase
+{
+	public function index($company, $attributes = ['where'=>[]])
+	{
+	    return view('estadisticas.pedidos.index',[
+	        'jurisdicciones'=>Jurisdicciones::whereHas('localidades', function($q){ $q->where('id_cliente', '=', 135); })->get()->pluck('jurisdiccion','id_jurisdiccion')->prepend('TODAS LAS JURISDICCIONES','-999'),
+	        'localidades' => Localidades::where('tipo',0)->where('id_cliente',135)->where('estatus',1)->get()->pluck('localidad','id_localidad')->prepend('TODOS LOS CENTROS DE SALUD','-999'),
+	    ]);
+	}
+	
+	public function store(Request $request, $company)
+	{
+	    $fecha_inicio = isset($request->datetimepicker1) ? $request->datetimepicker1 : '1900-01-01';
+	    $fecha_fin = isset($request->datetimepicker2) ? $request->datetimepicker2 : '1900-01-01';
+	    $jurisdiccion = isset($request->jurisdiccion) ? $request->jurisdiccion : -999;
+	    $localidad = isset($request->localidad) ? $request->localidad : -999;
+	    
+	    $char1 = DB::table('inv_pedido as p')
+    	    ->leftJoin('inv_pedido_detalle as d','d.id_pedido','p.id_pedido')
+    	    ->leftJoin('cat_localidad as l', function($q) use($localidad){
+    	        $q->on('l.id_localidad','=','p.id_localidad');
+    	        $q->whereRaw("(l.id_localidad = $localidad or $localidad = -999)");
+    	    })
+    	    ->leftJoin('cat_jurisdiccion as j','j.id_jurisdiccion','l.id_jurisdiccion')
+    	    ->leftJoin('cat_cuadro_producto as c', function($q) {
+    	        $q->on('c.id_cuadro','=','d.id_cuadro');
+    	        $q->whereRaw('c.clave_cliente = d.clave_cliente');
+    	    })
+    	    ->selectRaw('j.id_jurisdiccion, j.jurisdiccion, sum(d.cantidad_pedida)::numeric(10,0) as cantidad_pedida, sum(d.cantidad_surtida)::numeric(10,0) as cantidad_entregada,
+                (sum(d.cantidad_pedida) - sum(d.cantidad_surtida))::numeric(10,0) as diferencia_cantidad,
+                sum(d.cantidad_pedida * c.precio)::money::numeric as monto_pedido, sum(d.cantidad_surtida * c.precio)::money::numeric as monto_entregado,
+                (sum(d.cantidad_pedida * c.precio) - sum(d.cantidad_surtida * c.precio))::money::numeric as diferencia_monto')
+    	    ->where('l.id_cliente',135)
+    	    ->where('l.tipo',0)
+    	    ->where('l.estatus',1)
+    	    ->whereNotNull('d.clave_cliente')
+    	    ->whereBetween(DB::RAW("to_char(p.fecha_pedido, 'YYYY-MM-DD')"), [$fecha_inicio, $fecha_fin])
+    	    ->whereraw("(j.id_jurisdiccion = $jurisdiccion or $jurisdiccion = -999)")
+    	    ->groupBy(['j.id_jurisdiccion','j.jurisdiccion'])
+    	    ->orderBy('jurisdiccion')->get();
+	 
+	    $char01 = DB::table('inv_pedido as p')
+    	    ->leftJoin('inv_pedido_detalle as d','d.id_pedido','p.id_pedido')
+    	    ->leftJoin('cat_localidad as l', function($q) use($localidad){
+    	        $q->on('l.id_localidad','=','p.id_localidad');
+    	        $q->whereRaw("(l.id_localidad = $localidad or $localidad = -999)");
+    	    })
+    	    ->leftJoin('cat_jurisdiccion as j','j.id_jurisdiccion','l.id_jurisdiccion')
+    	    ->leftJoin('cat_cuadro_producto as c', function($q) {
+    	        $q->on('c.id_cuadro','=','d.id_cuadro');
+    	        $q->whereRaw('c.clave_cliente = d.clave_cliente');
+    	    })
+    	    ->selectRaw('j.jurisdiccion, l.localidad as centro_salud, sum(d.cantidad_pedida)::numeric(10,0) as cantidad_pedida, sum(d.cantidad_surtida)::numeric(10,0) as cantidad_entregada,
+                (sum(d.cantidad_pedida) - sum(d.cantidad_surtida))::numeric(10,0) as diferencia_cantidad,
+                sum(d.cantidad_pedida * c.precio)::money::numeric as monto_pedido, sum(d.cantidad_surtida * c.precio)::money::numeric as monto_entregado,
+                (sum(d.cantidad_pedida * c.precio) - sum(d.cantidad_surtida * c.precio))::money::numeric as diferencia_monto')
+                ->where('l.id_cliente',135)
+                ->where('l.tipo',0)
+                ->where('l.estatus',1)
+                ->whereNotNull('d.clave_cliente')
+                ->whereBetween(DB::RAW("to_char(p.fecha_pedido, 'YYYY-MM-DD')"), [$fecha_inicio, $fecha_fin])
+                ->whereraw("(j.id_jurisdiccion = $jurisdiccion or $jurisdiccion = -999)")
+                ->groupBy(['j.jurisdiccion','l.localidad'])
+                ->orderBy('jurisdiccion')->get();
+	    
+	    $char2 = DB::table('inv_pedido as p')
+    	    ->leftJoin('inv_pedido_detalle as d','d.id_pedido','p.id_pedido')
+    	    ->leftJoin('cat_localidad as l', function($q) use($localidad){
+    	        $q->on('l.id_localidad','=','p.id_localidad');
+    	        $q->whereRaw("(l.id_localidad = $localidad or $localidad = -999)");
+    	    })
+    	    ->leftJoin('cat_jurisdiccion as j','j.id_jurisdiccion','l.id_jurisdiccion')
+    	    ->leftJoin('cat_cuadro_producto as c', function($q) {
+    	        $q->on('c.id_cuadro','=','d.id_cuadro');
+    	        $q->whereRaw('c.clave_cliente = d.clave_cliente');
+    	    })
+    	    ->selectRaw("d.clave_cliente, substring(c.descripcion from 1 for 200) as producto, sum(d.cantidad_pedida) as cantidad_pedida, sum(d.cantidad_pedida * c.precio)::numeric(10,2) as monto, concat('#',substring(md5(random()::text) from 4 for 6)) as color")
+    	    ->where('l.id_cliente',135)
+    	    ->where('l.tipo',0)
+    	    ->where('l.estatus',1)
+    	    ->whereNotNull('d.clave_cliente')
+    	    ->whereBetween(DB::RAW("to_char(p.fecha_pedido, 'YYYY-MM-DD')"), [$fecha_inicio, $fecha_fin])
+    	    ->whereraw("(j.id_jurisdiccion = $jurisdiccion or $jurisdiccion = -999)")
+    	    ->groupBy(['d.clave_cliente','c.descripcion'])
+    	    ->orderBy('cantidad_pedida','desc')->limit(10)->get();
+    	    
+	    $char3 = DB::table('inv_pedido as p')
+    	    ->leftJoin('inv_pedido_detalle as d','d.id_pedido','p.id_pedido')
+    	    ->leftJoin('cat_localidad as l', function($q) use($localidad){
+    	        $q->on('l.id_localidad','=','p.id_localidad');
+    	        $q->whereRaw("(l.id_localidad = $localidad or $localidad = -999)");
+    	    })
+    	    ->leftJoin('cat_jurisdiccion as j','j.id_jurisdiccion','l.id_jurisdiccion')
+    	    ->leftJoin('cat_cuadro_producto as c', function($q) {
+    	        $q->on('c.id_cuadro','=','d.id_cuadro');
+    	        $q->whereRaw('c.clave_cliente = d.clave_cliente');
+    	    })
+    	    ->selectRaw("d.clave_cliente, substring(c.descripcion from 1 for 200) as producto, sum(d.cantidad_pedida * c.precio)::numeric(10,2) as monto, sum(d.cantidad_pedida) as cantidad, concat('#',substring(md5(random()::text) from 4 for 6)) as color")
+    	    ->where('l.id_cliente',135)
+    	    ->where('l.tipo',0)
+    	    ->where('l.estatus',1)
+    	    ->whereNotNull('d.clave_cliente')
+    	    ->whereBetween(DB::RAW("to_char(p.fecha_pedido, 'YYYY-MM-DD')"), [$fecha_inicio, $fecha_fin])
+    	    ->whereraw("(j.id_jurisdiccion = $jurisdiccion or $jurisdiccion = -999)")
+    	    ->groupBy(['d.clave_cliente','c.descripcion'])
+    	    ->orderBy('monto','desc')->limit(10)->get();
+	    
+	    return view('estadisticas.pedidos.index',[
+	        'jurisdicciones'=>Jurisdicciones::whereHas('localidades', function($q){ $q->where('id_cliente', '=', 135); })->get()->pluck('jurisdiccion','id_jurisdiccion')->prepend('TODAS LAS JURISDICCIONES','-999'),
+	        'localidades' => Localidades::where('id_cliente','=',135)->get()->pluck('localidad','id_localidad')->prepend('TODAS LAS LOCALIDADES','-999'),
+	        'char1' => $char1,
+	        'char01' => $char01,
+	        'char2' => $char2,
+	        'char3' => $char3,
+	    ]);
+	}
+	
+	public function getLocalidades($company, Request $request)
+	{
+	    $jurisdiccion = $request->jurisdiccion;
+	    $localidades = Localidades::where('tipo',0)->where('id_cliente',135)->where('estatus',1)
+            ->whereraw("(id_jurisdiccion = $jurisdiccion or $jurisdiccion = -999)")
+            ->get()->pluck('localidad','id_localidad')->prepend('TODOS LOS CENTROS DE SALUD','-999');
+	    return json_encode($localidades->toJson());
+	}
+}
---
+++ public/js/estadistica-requisiciones.js
@@ -0,0 +1,245 @@
+$('select[name="jurisdiccion"]').on('change', function() {
+    // alert($("#id_localidad").data('url'));
+    var value = $(this).val();
+    // alert(id_localidad);
+    if(value) {
+        $.ajax({
+            type: "POST",
+            url: $("#jurisdiccion").data('url'),
+            data: 'jurisdiccion='+value,
+            dataType: "json",
+            success:function(data) {
+                datos = $.parseJSON(data);
+                $('select[name="localidad"]').empty();
+                $.each(datos, function(key, value) {
+                    $('select[name="localidad"]').append('<option value="'+ key +'">'+ value +'</option>');
+                });
+                $('select[name="localidad"]').val(-999);
+            }
+        });
+
+    }else{
+        $('select[name="localidad"]').empty();
+    }
+});
+
+var chart = AmCharts.makeChart("char-1", {
+    "theme": "light",
+    "type": "serial",
+    "dataProvider": chart1,
+    "valueAxes": [{
+        //"unit": "%",
+        "position": "left",
+        "title": "Pedido vs Entregado / Jurisdiccion",
+    }],
+    "startDuration": 1,
+    "graphs": [{
+        "balloonText": "Pedido para [[category]]: <b>[[value]] - $[[monto_entregado]]</b>",
+        "fillAlphas": 0.9,
+        "lineAlpha": 0.2,
+        "title": "Pedido",
+        "type": "column",
+        "valueField": "cantidad_pedida",
+        "labelText": "[[value]]",
+    }, {
+        "balloonText": "Entregado para [[category]]: <b>[[value]] - $[[monto_entregado]]</b>",
+        "fillAlphas": 0.9,
+        "lineAlpha": 0.2,
+        "title": "Entregado",
+        "type": "column",
+        "clustered":false,
+        "columnWidth":0.5,
+        "valueField": "cantidad_entregada",
+        "labelText": "<br>[[value]]",
+    }],
+    "legend": {
+        "useGraphSettings": true
+      },
+    "plotAreaFillAlphas": 0.1,
+    "categoryField": "jurisdiccion",
+    "categoryAxis": {
+        "gridPosition": "start",
+        "labelRotation": 45,
+    },
+    "export": {
+    	"enabled": true
+     }
+});
+
+var chart = AmCharts.makeChart( "char-01", {
+	"theme": "light",
+    "type": "serial",
+    "dataProvider": chart2,
+    "valueAxes": [{
+        //"unit": "%",
+        "position": "left",
+        "title": "Pedido vs Entregado / Centro Salud",
+    }],
+    "startDuration": 1,
+    "graphs": [{
+        "balloonText": "Pedido para [[category]]: <b>[[value]] - $[[monto_pedido]]</b>",
+        "fillAlphas": 0.9,
+        "lineAlpha": 0.2,
+        "title": "Pedido",
+        "type": "column",
+        "valueField": "cantidad_pedida",
+        "labelText": "[[value]]",
+        "fillColors": "#EFD216",
+    }, {
+        "balloonText": "Entregado para [[category]]: <b>[[value]] - $[[monto_entregado]]</b>",
+        "fillAlphas": 0.9,
+        "lineAlpha": 0.2,
+        "title": "Entregado",
+        "type": "column",
+        "clustered":false,
+        "columnWidth":0.5,
+        "valueField": "cantidad_entregada",
+        "labelText": "<br>[[value]]",
+        "fillColors": "#76BEDF",
+    }],
+    "legend": {
+        "useGraphSettings": true
+      },
+    "plotAreaFillAlphas": 0.1,
+    "categoryField": "centro_salud",
+    "categoryAxis": {
+        "gridPosition": "start",
+    	"labelRotation": 25,
+    },
+    "export": {
+    	"enabled": true
+     }
+});
+
+var chart = AmCharts.makeChart( "char-02", {
+    "type": "pie",
+    "theme": "light",
+    "innerRadius": 80,
+    "dataProvider": chart5,
+    "valueField": "cantidad",
+    "titleField": "area",
+    "outlineAlpha": 0.4,
+	    "depth3D": 15,
+	  	"angle": 50,
+	  	"pullOutRadius": 60,
+	  	"marginTop": 20,
+	  	"labelText": "[[area]] : [[percents]]%",
+	    "balloonText": "<b>[[area]] :<b>[[value]]</b> ([[percents]]%)</span>",
+    "balloon":{
+    	"fixedPosition":false
+    },
+    "export": {
+    	"enabled": true
+    }
+});
+/*
+var chart = AmCharts.makeChart( "char-02", {
+    "type": "serial",
+    "theme": "light",
+    "rotate": true,
+    "dataProvider": chart5,
+    "valueAxes": [ {
+        "gridColor": "#FFFFFF",
+        "gridAlpha": 0.2,
+        "dashLength": 0
+      } ],
+      "gridAboveGraphs": true,
+      "startDuration": 1,
+      "depth3D": 20,
+  	  "angle": 30,
+      "graphs": [ {
+    	"fillColorsField": "color",
+        "balloonText": "[[area]]: <b>[[cantidad_pedida]]</b>",
+        "fillAlphas": 0.8,
+        "lineAlpha": 0.2,
+        "type": "column",
+        "valueField": "cantidad_pedida",
+        "labelText": "[[value]]",
+      } ],
+      "chartCursor": {
+        "categoryBalloonEnabled": false,
+        "cursorAlpha": 0,
+        "zoomable": false
+      },
+      "categoryField": "area",
+      "categoryAxis": {
+        "gridPosition": "start",
+        "gridAlpha": 0,
+        "tickPosition": "start",
+        "tickLength": 20
+      },
+      "export": {
+    	"enabled": true
+      }
+});
+*/
+var chart = AmCharts.makeChart("char-2", {
+    "theme": "light",
+    "type": "serial",
+    "startDuration": 2,
+    "dataProvider": chart3,
+    "valueAxes": [{
+        "position": "left",
+        "axisAlpha":0,
+        "gridAlpha":0
+    }],
+    "graphs": [{
+        "balloonText": "[[category]]: [[producto]] <b>[[value]]</b>",
+        "colorField": "color",
+        "fillAlphas": 0.85,
+        "lineAlpha": 0.1,
+        "type": "column",
+        "topRadius":1,
+        "valueField": "cantidad_pedida",
+        "labelText": "[[value]]",
+    }],
+    "depth3D": 40,
+	"angle": 30,
+    "chartCursor": {
+        "categoryBalloonEnabled": false,
+        "cursorAlpha": 0,
+        "zoomable": false
+    },
+    "categoryField": "clave_cliente",
+    "categoryAxis": {
+        "gridPosition": "start",
+        "labelRotation": 45,
+        "axisAlpha":0,
+        "gridAlpha":0
+
+    },
+    "export": {
+    	"enabled": true
+     }
+});
+
+var chart = AmCharts.makeChart("char-3", {
+    "theme": "light",
+    "type": "serial",
+	"startDuration": 2,
+    "dataProvider": chart4,
+    "graphs": [{
+        "balloonText": "[[category]]: <b>[[value]]</b>",
+        "fillColorsField": "color",
+        "fillAlphas": 0.59,
+        "lineAlpha": 0.1,
+        "type": "column",
+        "valueField": "monto",
+        "labelText": "$ [[value]]",
+    }],
+    "depth3D": 20,
+	"angle": 30,
+    "chartCursor": {
+        "categoryBalloonEnabled": false,
+        "cursorAlpha": 0,
+        "zoomable": false
+    },
+    "categoryField": "clave_cliente",
+    "categoryAxis": {
+        "gridPosition": "start",
+        "labelRotation": 30
+    },
+    "export": {
+    	"enabled": true
+     }
+});
